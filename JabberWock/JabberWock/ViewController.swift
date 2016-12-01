005626adf5552c155c8e5c83c606f9e75df534a0
kazuyuki shimizu <kazuyukishimizu@me.com>
2016-12-01 16:24:47 +0900

    renew project

---
+++ JabberWock/JabberWock/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JabberWock
+//
+//  Created by 清水 一征 on 2016/12/01.
+//  Copyright © 2016年 momiji-mac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
