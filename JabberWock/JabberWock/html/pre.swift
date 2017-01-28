5c0dc45bf2665af30633beb2ff1d374cbab5dcc0
kazuyuki shimizu <kazuyukishimizu@me.com>
2017-01-28 17:30:40 +0900

    new file:   JabberWock/html/pre.swift

---
+++ JabberWock/JabberWock/html/pre.swift
@@ -0,0 +1,21 @@
+//
+//  pre.swift
+//  JabberWock
+//
+//  Created by 清水 一征 on 2017/01/28.
+//  Copyright © 2017年 momiji-mac. All rights reserved.
+//
+
+import Foundation
+
+class PRE:JWMulti {
+    
+    override init() {
+        super.init()
+        setName(name: "pre")
+    }
+    
+    
+    
+    
+}
